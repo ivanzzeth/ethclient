c62e63811d44a34bae9af4ec50af7ca3bc58fc54
ivanzz.eth <ivanzz.eth@gmail.com>
2026-03-13 15:15:01 +0800

    Add DialWithTransport

--- dialer.go
+++ dialer.go
@@ -1,14 +1,15 @@
 package ethclient
 
 import (
-	"github.com/ethereum/go-ethereum/rpc"
-
+	"net/http"
+	"net/url"
 	"sync"
 	"time"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/rpc"
 )
 
 type EthClientInterface interface {
@@ -78,4 +79,22 @@ func Dial(rawurl string) (*Client, error) {
 
 	client := NewClient(rpcClient)
 	return client, nil
+}
+
+// DialWithTransport creates an RPC client that uses the given transport for HTTP/HTTPS endpoints.
+// When transport is non-nil and rawurl has scheme "http" or "https", rpc.DialHTTPWithClient is used.
+// Otherwise Dial(rawurl) is used (e.g. for ws/wss or when transport is nil).
+func DialWithTransport(rawurl string, transport *http.Transport) (*Client, error) {
+	if transport != nil {
+		u, err := url.Parse(rawurl)
+		if err == nil && (u.Scheme == "http" || u.Scheme == "https") {
+			httpClient := &http.Client{Transport: transport}
+			rpcClient, err := rpc.DialHTTPWithClient(rawurl, httpClient)
+			if err != nil {
+				return nil, err
+			}
+			return NewClient(rpcClient), nil
+		}
+	}
+	return Dial(rawurl)
 }
